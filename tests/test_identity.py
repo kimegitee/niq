e87fad0a7ee078fa770a2af083e587b75eb02674
Nhat Bui <kimegitee@gmail.com>
2019-10-21 00:05:56 +0700

    Add decorating methods test cases

--- tests/test_identity.py
+++ tests/test_identity.py
@@ -1,19 +1,36 @@
+import os
+import time
+import pytest
 from niq import cache
+from niq import identify_func
 from tempfile import TemporaryDirectory
 
 
-def identity(x):
-    return x
+os.environ['NIQ_CACHE'] = '1'
 
 
-def test_decorating_with_custom_cache_dir():
-    f = identity
-    x = 1
+@pytest.fixture
+def d():
     with TemporaryDirectory() as d:
-        assert cache(cache_dir=d)(f)(x) == f(x)
+        yield d
 
+def now():
+    return time.time()
 
-def test_decorating_without_custom_cache_dir():
-    f = identity
-    x = 1
-    assert cache(f)(x) == f(x)
+def test_custom_cache_dir(d):
+    f = cache(cache_dir=d)(now)
+    assert f() == f()
+
+def test_caching_bound_method(d):
+    class C:
+        def f(_):
+            return now()
+    f = cache(cache_dir=d)(C().f)
+    assert f() == f()
+
+def test_caching_unbound_method(d):
+    class C:
+        @cache(cache_dir=d)
+        def f(_):
+            return now()
+    assert C().f() == C().f()
