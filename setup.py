5f46fddeb89b190fd4982039631181168e0bf2d2
Nhat Bui (Nick) <kimegitee@gmail.com>
2020-06-18 21:17:22 +0700

    Bump to v1.2.2 (#5)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='niq',
-    version='1.2.1',
+    version='1.2.2',
     description="Nick's library of extraneous utils",
     url='https://github.com/Cinnamon/niq',
     author='kimegitee',
