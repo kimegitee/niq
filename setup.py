42f0dbfe32264dd4b2ce3859d2a588157e7ff936
Nhat Bui <kimegitee@gmail.com>
2021-01-08 18:05:57 +0700

    Add long description

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 import setuptools
 from setuptools import setup
 
+with open('README.md') as f:
+    long_description = f.read()
+
 setup(
     name='niq',
     version='1.2.2',
     description="Nick's library of extraneous utils",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/kimegitee/niq',
     author='kimegitee',
     author_email='kimegitee@gmail.com',
