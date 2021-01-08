62b9845ddbb0ad72a08b85dee6d8de5b64064bf1
Nhat Bui (Nick) <kimegitee@gmail.com>
2021-01-08 18:30:16 +0700

    Publish (#1)
    * Fix urls
    
    * Add long description
    
    * Remove binary
    
    * Update instruction

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
+import setuptools
 from setuptools import setup
+
+with open('README.md') as f:
+    long_description = f.read()
 
 setup(
     name='niq',
     version='1.2.2',
     description="Nick's library of extraneous utils",
-    url='https://github.com/Cinnamon/niq',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/kimegitee/niq',
     author='kimegitee',
     author_email='kimegitee@gmail.com',
     license='MIT',
     packages=setuptools.find_packages(),
     install_requires=['xxhash>=1.4.0,<2', 'joblib>=0.14.0,<0.15'],
-    scripts=['bin/niq'],
     zip_safe=False,
 )
